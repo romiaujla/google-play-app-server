1b5d22407986c0be85f9b4360a90392c6affcff1
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-09-04 16:50:42 -0400

    created tests for sorting and genres parameters

--- test/app.test.js
+++ test/app.test.js
@@ -22,7 +22,7 @@ const GENRES = [
     'strategy', 
     'casual', 
     'arcade', 
-    'card'
+    'card',
 ];
 const SORT = [
     'rating', 
@@ -38,6 +38,7 @@ describe(`GET /apps`, () => {
             .expect('Content-Type', /json/)
             .then((res) => {
                 expect(res.body).to.be.an('array');
+                expect(res.body.length >= 1).to.be.true;
                 const app = res.body[0];
                 expect(app).to.include.all.keys(APPKEYS);
             });
@@ -55,5 +56,86 @@ describe(`GET /apps`, () => {
             .get('/apps')
             .query({genres: 'incorrect'})
             .expect(400, `Genres must be one of ${GENRES.join(', ')}`);
-    })
-})
+    });
+
+    it(`Sort correctly when rating parameter is passed`, () => {
+        return request(app)
+            .get('/apps')
+            .query({sort: 'rating'})
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.an('array');
+                const ratingArray = res.body.map((app) => app.Rating);
+                checkSorting(ratingArray);
+            });
+    });
+
+    it(`Sort correctly when app parameter is passed`, () => {
+        return request(app)
+            .get('/apps')
+            .query({sort: 'app'})
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.an('array');
+                const ratingArray = res.body.map((app) => app.App);
+                checkSorting(ratingArray);
+            });
+    });
+
+    it(`Get correct results when genres parameter is passed`, () => {
+        const expectedGenre = 4; // any number <= 5
+        return request(app)
+            .get('/apps')
+            .query({genres: GENRES[expectedGenre]})
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.an('array');
+                const genreArray = res.body.map((app) => app.Genres);
+                console.log(genreArray);
+                matchGenres(genreArray, expectedGenre);
+            });
+    });
+
+    it(`Gets correct result with both the parameters`, () => {
+        
+        const expectedSort = 0; // any number <= 1
+        const expectedGenre = 4; // any number <= 5
+
+        return request(app)
+            .get('/apps')
+            .query({
+                sort: SORT[expectedSort],
+                genres: GENRES[expectedGenre]
+            })
+            .expect(200)
+            .then((res) => {
+                let sortArray = [];
+                expect(res.body).to.be.an('array');
+                
+                // capitalize the sort parameter for correct sorting
+                sort = SORT[expectedSort][0].toUpperCase() + SORT[expectedSort].substr(1);
+
+                const genreArray = res.body.map((app) => app.Genres);
+                const sortingArray = res.body.map((app) => app[sort]);
+                matchGenres(genreArray, expectedGenre);
+                checkSorting(sortingArray);
+            })
+    });
+
+    // a function that checks if the sort is correct
+    checkSorting = (sortArray) => {        
+        let sortIsCorrect = true;
+        for(let i = 1; i < sortArray.length; i++){
+            sortIsCorrect = sortArray[i-1] <= sortArray[i];
+        }
+        expect(sortIsCorrect).to.be.true;
+    }
+
+    // a function that checks if the genre is correct
+    matchGenres = (genreArray, expectedGenre) => {
+        genreArray.map((genre) => {
+            expect(genre.toLowerCase().includes(GENRES[expectedGenre])).to.be.true;
+        })
+    }
+
+});
